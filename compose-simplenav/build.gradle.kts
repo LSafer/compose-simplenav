dddbf1e8badae07ce3f0605213f20cb453357774
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2025-10-25 14:41:33 +0300

    build: enabled maven auto release

--- compose-simplenav/build.gradle.kts
+++ compose-simplenav/build.gradle.kts
@@ -78,7 +78,7 @@ android {
 }
 
 mavenPublishing {
-    publishToMavenCentral()
+    publishToMavenCentral(automaticRelease = true)
     signAllPublications()
     coordinates(
         groupId = group.toString(),
